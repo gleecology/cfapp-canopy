7ecda1142056bcc2d405c383e357c019250b7837
Greg Lee Coleman <gleeco@cloudflare.com>
2013-02-13 11:14:07 -0800

    pared down and ready for use

--- public/javascripts/canopy.js
+++ public/javascripts/canopy.js
@@ -1,21 +1,20 @@
-CloudFlare.define( 'canopy', [       'canopy/config', 'cloudflare/dom',   'cloudflare/user',  'cloudflare/owl',       'cloudflare/jquery1.7',     'cloudflare/console' ], 
-                            function(cfg,           dom,                user,               owl,                    jQuery,                     console ) {
 
-    var $       = jQuery,
-        appName = 'canopy',
-        D       = 1,
-        cfOwl   = owl.createDispatcher(appName);
+CloudFlare.define( 'canopy', [      'canopy/config',   'cloudflare/jquery1.7' ],
+                        function(   cfg,               jQuery ){ 
+    var  appName = 'canopy',
+        $       = jQuery;
 
-    console.log( 'owl created cfOwl' , cfOwl );
+    cfg         = cfg || {};
+    cfg.crid    =  cfg.crid ||  '537005147';    // adtype
+    cfg.cid     =  cfg.publisher_id || '8CUR5F7GW';     // pub id
+    cfg.size    = '641x481';
 
     $(document).ready( function(){
-        var sct     = document.createElement("script"),
-            sctHl   = document.getElementsByTagName("script")[0];
-        sct.type    = "text/javascript";
-        sct.src     = 'http://contextual.media.net/inslmedianet.js?cid=8CUR5F7GW&crid=537005147&size=640x480';
-        sct.async   = "async";
-        sctHl.parentNode.insertBefore(sct, sctHl);
+        var sct = document.createElement("script"),
+        sctHl = document.getElementsByTagName("script")[0];
+        sct.type = "text/javascript";
+        sct.src = 'http://contextual.media.net/inslmedianet.js?cid=' + cfg.cid + '&crid=' + cfg.crid + '&size=' + cfg.size;
+        sct.async = "async";
+        sctHl.parentNode.insertBefore(sct, sctHl)
     });
-
-    console.log(appName + ' code complete' );
 } );
